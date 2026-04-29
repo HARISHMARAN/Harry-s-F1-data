3936d78cbcf4dcbd0619563c2628e62e22f05d62
Harishmaran <harishmaran@Mac.lan>
2026-04-30 00:48:37 +0530

    Wrap homepage search params in suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import App from "../src/App";
 
 export default function HomePage() {
-  return <App />;
+  return (
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+  );
 }
